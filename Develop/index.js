f12f2b6290277056ed45993773e499d938fb6036
Gurveer Madurai <maduraigurveer@gmail.com>
2023-04-28 16:54:09 -0700

    questions added to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of the project?',
+    'Write a description of the project',
+    'What are the installation instructions?',
+    'Usage information:',
+    'Contribution guidelines:',
+    'Test instructions:',
+    'Choose a license for the application:',
+    'Enter your GitHub username:',
+    'Enter your email address:',
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
